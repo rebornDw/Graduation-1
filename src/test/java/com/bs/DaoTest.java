fa85887923862a75a86d75e4dd9d0fcf7147c228
jiawei.wang2 <jiawei.wang2@baozun.com>
2018-08-29 14:59:36 +0800

    添加测试类

---
+++ src/test/java/com/bs/DaoTest.java
@@ -0,0 +1,43 @@
+package com.bs;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.bs.work.Dao.UserDao;
+import com.bs.work.model.User;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DaoTest {
+	
+	private static final Logger log = LoggerFactory.getLogger(DaoTest.class);
+
+	@Autowired
+	private UserDao userDao;
+	
+	@Test
+	public void getUserById() {
+		//输入需要查询的ID
+		Integer id = 2;
+		User user = userDao.getUserById(id);
+		log.info("id为"+id+"的用户信息："+user);
+	}
+	
+	@Test
+	public void getUserList() {
+		List<User> userList = userDao.getUserList();
+		for(int i=0;i<=userList.size();i++){
+			User user = userList.get(i);
+			log.info("的用户信息："+user);
+		}
+	}
+
+	
+}
---
+++ src/test/java/com/bs/ManagerTest.java
@@ -0,0 +1,30 @@
+package com.bs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.bs.work.Manager.UserManager;
+import com.bs.work.model.User;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManagerTest {
+
+	private static final Logger log = LoggerFactory.getLogger(ManagerTest.class);
+	
+	@Autowired
+	private UserManager userManager;
+	
+	@Test
+	public void login() {
+		int id = 1;
+		User user = userManager.getUserById(id);
+		log.info("id为"+id+"的用户信息："+user);
+	}
+
+}
